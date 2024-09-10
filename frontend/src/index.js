1e5eacde684138aefda45302b25d352664f990ec
imHeeshan <imHeeshan@gmail.com>
2024-09-10 09:42:21 +0530

    initial Commit

---
+++ frontend/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-dropdown/style.css';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <>
+    <App />
+  </>
+);
+
